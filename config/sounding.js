71ff67f26651376db23c509735177eeaf0535215
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2026-08-24 22:08:34 +0100

    test(e2e): isolate Sounding app lifecycles (#487)

--- config/sounding.js
+++ config/sounding.js
@@ -1,14 +1,7 @@
-const soundingPort =
-  Number(process.env.SOUNDING_PORT) || 30000 + (process.pid % 20000)
-
 module.exports.sounding = {
   app: {
     liftOptions: {
       explicitHost: '127.0.0.1',
-      port: soundingPort,
-      custom: {
-        baseUrl: `http://127.0.0.1:${soundingPort}`
-      },
       hooks: {
         sockets: true
       }
